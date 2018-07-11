89da9180be2602f8b8b0d0df33ba82d3ae7e8130
Richard Morrisby <rmorrisby@gmail.com>
2018-07-11 08:04:20 +0100

    Initial commit

---
+++ Euler1/Euler1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//Solution for Project Euler problem 1
+
+namespace Euler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
